adf3bc3de033219b4a3918211492e505475c4c5a
Isabella <24170045@uttcampus.edu.mx>
2026-07-05 13:17:43 -0600

    Aguegue metodo ToString en cada clase que hereda de CuentaUsuario, para imprimirlas

--- src/CuentaEstandar.java
+++ src/CuentaEstandar.java
@@ -3,4 +3,8 @@ public class CuentaEstandar extends CuentaUsuario{
     public CuentaEstandar(String correoElectronico, int mesesActivo) {
         super(correoElectronico,mesesActivo,new PlanEstandar());
     }
+    @Override
+    public String toString() {
+        return "CuentaEstandar{}";
+    }
 }
--- src/CuentaPremium.java
+++ src/CuentaPremium.java
@@ -3,4 +3,9 @@ public class CuentaPremium extends CuentaUsuario {
     public CuentaPremium(String correoElectronico, int mesesActivo) {
         super(correoElectronico,mesesActivo,new PlanPremium());
     }
+
+    @Override
+    public String toString() {
+        return "CuentaPremium{}";
+    }
 }
