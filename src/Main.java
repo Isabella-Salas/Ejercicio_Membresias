5940b96d15513c3125f8e31c9b8937925e38293c
Isabella <24170045@uttcampus.edu.mx>
2026-07-05 13:13:44 -0600

    Agregue a menu opcion de seleccionar y agregar datos de tipo de cuenta, junto con la opcion 1 de registrar cuentaBasica

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,31 @@ public class Main {
 
         do{
             System.out.println("Seleccione la accion que desea realizar: ");
-            System.out.println("1.---");
+            System.out.println("1.Cuenta usuario");
             System.out.println("2.---");
             System.out.println("3.Salir");
             opcion = sc.nextInt();
             switch (opcion){
 
                 case 1:
+                    System.out.println("Seleccione el tipo de cuenta: ");
+                    System.out.println("1.Basica");
+                    System.out.println("2.Estandar");
+                    System.out.println("3.Premium");
+                    int opcionCuenta = sc.nextInt();
+                    switch (opcionCuenta){
+                        case 1:
+                            System.out.println("Ingrese el correo electronico: ");
+                            String correoElectronico = sc.next();
+                            System.out.println("Ingrese el numero de meses activos: ");
+                            int mesesActivo = sc.nextInt();
+                            CuentaBasica cuentaBasica = new CuentaBasica(correoElectronico,mesesActivo);
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                    }
                     break;
                 case 2:
                     break;
