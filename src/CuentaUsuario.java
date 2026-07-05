eae712e0c70fdba3c13e9f336a4ba06ec88f2999
Isabella <24170045@uttcampus.edu.mx>
2026-07-05 10:48:42 -0600

    Agregue constructo que recibe el correo, los meses activo y el plan de suscripcion, junto con el metodo obtenerTotalAPagar,que llamata al metodo de PlanSuscripcion

--- src/CuentaUsuario.java
+++ src/CuentaUsuario.java
@@ -3,4 +3,15 @@ public abstract class CuentaUsuario {
     private String correoElectronico;
     private int mesesActivo;
     private PlanSuscripcion plan;
+
+
+    public CuentaUsuario(String correoElectronico, int mesesActivo, PlanSuscripcion plan) {
+        this.correoElectronico = correoElectronico;
+        this.mesesActivo = mesesActivo;
+        this.plan = plan;
+    }
+
+    public Double obtenerTotalAPagar(){
+        return plan.calcularCosto(mesesActivo);
+    }
 }
